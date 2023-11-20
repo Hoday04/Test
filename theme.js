9217735d778d2506ebc4da2ef32ea87b376b40e1
Hoday04 <103636616+Hoday04@users.noreply.github.com>
2023-11-20 16:24:54 +0300

    fix: fix the color change of the difficulty button and the condition

--- theme.js
+++ theme.js
@@ -3,20 +3,25 @@ function LightTheme(){
         bg.classList.toggle('BG');
         bg.classList.toggle('light-theme');
         bgcolor = light;
+        colorSelCompl = "olive";
         localStorage.setItem("theme","light-theme");
     }
     else{
         bg.classList.toggle('light-theme');
         bg.classList.toggle('BG');
         bgcolor = dark;
+        colorSelCompl = "teal";
         localStorage.setItem("theme","BG");
     }
     let buts = document.querySelectorAll("button");
     for(let i=0;i<buts.length;i++){
-        if(buts[i].style.backgroundColor==="red" || buts[i].style.backgroundColor==="green"){
-        }
-        else{
-            buts[i].style.backgroundColor = bgcolor;
+        if(buts[i].style.backgroundColor!==colorFalseAns && buts[i].style.backgroundColor!==colorRightAns){
+            if(buts[i].style.backgroundColor==="teal" || buts[i].style.backgroundColor==="olive"){
+                buts[i].style.backgroundColor=colorSelCompl;
+            }
+            else{
+                buts[i].style.backgroundColor = bgcolor;
+            }
         }
     }
 }
